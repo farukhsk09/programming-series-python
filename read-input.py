e27de007d6157c39b74e540f6560a4f1b3864407
sameer sk <farukh.sk09@gmail.com>
2021-07-11 23:15:51 +0530

    Created read-input.py
    Reads int , float , string from input

---
+++ read-input.py
@@ -0,0 +1,9 @@
+# read line string
+name = input("name : ")
+print("hi %s" % name)
+# read int from input()
+age = int(input("your age :"))
+print("%s is aged %d" % (name, age))
+# read float from input()
+weight = float(input("your weight :"))
+print("%s at age %d is weighed at %f pounds" % (name, age, weight))
