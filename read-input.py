abae9e5eef5b5dcab3331f5c57debce2226ceb38
sameer sk <farukh.sk09@gmail.com>
2021-07-11 23:25:31 +0530

    Update read-input.py
    Updated with Reading arrays in python.
    * Using map and split to read input array of integers

--- read-input.py
+++ read-input.py
@@ -7,3 +7,11 @@ print("%s is aged %d" % (name, age))
 # read float from input()
 weight = float(input("your weight :"))
 print("%s at age %d is weighed at %f pounds" % (name, age, weight))
+
+# read array as a string and split based on a delimitter
+array_of_weights = input("enter weights : ").split(" ")
+# we needed a list of integers but we are seeing list of strings
+print(array_of_weights)
+array_of_weights = list(map(int, input("Enter weights :").split(" ")))
+print(array_of_weights)
+# read Line from string
